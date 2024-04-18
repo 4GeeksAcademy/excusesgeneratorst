2d6ac95d13ab0da4d34fae622ebe3e5bed4a4f53
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-04-18 12:03:21 +0000

    actualizado

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 /* eslint-disable */
 import "./style.css";
+
+let who = ["Annabelle","Rickroll","Windows","El innombrable"]
+let action = ["Se comió","Quemo","Hizo desaparecer","Hechizo"]
+let what = ["Mis deberes","El ordenador","Mi coche","A mi perro"]
+let when = ["Mientras dormia,","Cuando no estaba en casa","Mientras me duchaba"]
+
+
+
+ 
+
+
+
+function press(){
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    let excusa = document.getElementById("excusas")
+    who[randomNumber] =
+}
