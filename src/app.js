8904786f6c3c6614b37b3b1f3bdc79530013e443
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-04-18 15:05:45 +0000

    actualizado1

--- src/app.js
+++ src/app.js
@@ -1,19 +1,33 @@
 /* eslint-disable */
 import "./style.css";
 
-let who = ["Annabelle","Rickroll","Windows","El innombrable"]
-let action = ["Se comió","Quemo","Hizo desaparecer","Hechizo"]
-let what = ["Mis deberes","El ordenador","Mi coche","A mi perro"]
-let when = ["Mientras dormia,","Cuando no estaba en casa","Mientras me duchaba"]
+let who = ["Annabelle", "Rickroll", "Windows", "El Innombrable", "Sauron"];
+let action = ["se comió", "quemo", "hizo desaparecer", "hechizo", "robo"];
+let what = [
+  "mis deberes",
+  "el ordenador",
+  "mi coche",
+  "a mi perro",
+  "Los libros"
+];
+let when = [
+  "mientras dormia",
+  "cuando no estaba en casa",
+  "mientras me duchaba",
+  "mientras limpiaba",
+  "cuando sali a comprar"
+];
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = press();
+  });
+};
 
+function press() {
+  let quien = Math.round(Math.random() * 4);
+  let acto = Math.round(Math.random() * 4);
+  let que = Math.round(Math.random() * 4);
+  let cuando = Math.round(Math.random() * 4);
 
-
- 
-
-
-
-function press(){
-    let randomNumber = Math.floor((Math.random() * 3) + 1);
-    let excusa = document.getElementById("excusas")
-    who[randomNumber] =
+  return who[quien] + " " + action[acto] + " " + what[que] + " " + when[cuando];
 }
